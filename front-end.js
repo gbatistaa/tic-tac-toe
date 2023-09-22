860d53809afa2aa4480928313521fa77cffceede
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-09-21 23:52:59 -0300

    criação da função recursiva criaArray, que transforma uma HTMLColletion em uma array de elementos HTML com a mesma classe

---
+++ front-end.js
@@ -0,0 +1,10 @@
+// Função recursiva para transformar HTMLCollection (registro) em uma array de elementos da mesma classe HTML
+
+const criaArray = (classe, index = 0, array = []) => {
+    const key = index.toString();
+    if (index === 9) return array;
+    else {
+        const vetor = classe[key];
+        return criaArray (classe, index + 1, [...array, vetor]);
+    };
+};
