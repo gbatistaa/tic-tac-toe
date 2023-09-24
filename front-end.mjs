c3417a2cc8924d34553c4f158904b7778d2a3aab
PauloRugani <phrugnay12020511@gmail.com>
2023-09-23 23:20:24 -0300

    resolução do bug do hover personalizado e identificação das células selecioandas pelo jogador

--- front-end.mjs
+++ front-end.mjs
@@ -1,8 +1,14 @@
 // Atribuição de constantes dos elementos virtuais 
 const xSimbolo = document.createElement ('div');
 const oSimbolo = document.createElement ('div');
-const caixasPequenas = document.getElementsByClassName ('caixa-pequena');
+
+// Selecionando os elementos HTML e atribuindo a constantes
+const principal = document.getElementById('principal');
+const info = document.getElementById('info');
+const btInstrucao = document.getElementById('botao-instrucao');
+const btFechar = document.getElementById('botao-fechar');
 const regras = document.getElementById('info');
+const caixasPequenas = document.getElementsByClassName ('caixa-pequena');
 
 // Função recursiva para Adicionar simbolo x ou o na caixa pequena
 const adicionarEvento = (caixaP, index = 0) => { 
@@ -10,19 +16,21 @@ const adicionarEvento = (caixaP, index = 0) => {
     else  {
         const elemento = caixaP[index];
         elemento.addEventListener('click', () => {
-            console.log(elemento)
             const vez = elemento.classList[2];
-            if (vez === 'X'){
-                const simboloClone = xSimbolo.cloneNode(true); //o appendchild é tranferido para a proxima casa selecionada e não duplicado
+            if (vez === 'X') {
+                const simboloClone = xSimbolo.cloneNode(true); //com appendchild o X ou o O são tranferidos para a proxima casa selecionada e não duplicados
                 adicionarClasse('x-simbolo')(simboloClone);    //então é criado o simboloclone, para o X ou O ficarem nos lugares após
-                                                               //a próxima escolhida
-                elemento.appendChild(simboloClone);
+                elemento.appendChild(simboloClone);            //a próxima escolha
                 mudaVezX(caixasPequenas);
+                removerClasse(elemento.classList[2])(elemento)
+                adicionarClasse('X-add')(elemento);            //identifica um quadrante selecionado
             } else if (vez === 'O'){
                 const simboloClone = oSimbolo.cloneNode(true);
                 adicionarClasse('o-simbolo')(simboloClone);
                 elemento.appendChild(simboloClone);
                 mudaVezO(caixasPequenas);
+                removerClasse(elemento.classList[2])(elemento)
+                adicionarClasse('O-add')(elemento)             //identifica um quadrante selecioando   
             }
         }, {once: true});
         adicionarEvento(caixaP, index + 1);
@@ -30,20 +38,26 @@ const adicionarEvento = (caixaP, index = 0) => {
 };
 adicionarEvento(caixasPequenas);
 
+//verifica se o quadrante ja foi selecionado, caso não, muda a vez
 const mudaVezX = (objClasse, index = 0) => {
     if (index === objClasse.length) return true;
     else {
-        removerClasse('X')(objClasse[index])
-        adicionarClasse('O')(objClasse[index])
+        removerClasse('X')(objClasse[index]);
+        if (objClasse[index].classList.contains('X-add') === false) {
+            adicionarClasse('O')(objClasse[index]);
+        }
         return mudaVezX(objClasse, index + 1)
     }
 }
 
+//verifica se o quadrante ja foi selecionado, caso não, muda a vez
 const mudaVezO = (objClasse, index = 0) => {
     if (index === objClasse.length) return true;
     else {
-        removerClasse('O')(objClasse[index])
-        adicionarClasse('X')(objClasse[index])
+        removerClasse('O')(objClasse[index]);
+        if (objClasse[index].classList.contains('O-add') === false) {
+            adicionarClasse('X')(objClasse[index]);
+        }
         return mudaVezO(objClasse, index + 1)
     }
 }
@@ -69,11 +83,6 @@ const removerClasse = classe => elemento => {
     elemento.classList.remove(classe);
 }
 
-// Selecionando os elementos HTML e atribuindo a constantes
-const principal = document.getElementById('principal');
-const info = document.getElementById('info');
-const btInstrucao = document.getElementById('botao-instrucao');
-const btFechar = document.getElementById('botao-fechar');
 
 // "Ouve o evento click no botão de instrução"
 btInstrucao.addEventListener('click', () => {
